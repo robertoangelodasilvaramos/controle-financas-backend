5a52873abc7c6304af71c380ebbe13dab1480453
Roberto Angelo da Silva Ramos <sdinformatia@gmail.com>
2021-01-03 14:09:05 -0300

    meu primeiro commit

---
+++ src/test/java/com/sdinfor/controlefinancas/ControleFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdinfor.controlefinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
