e021f5dc183c02a485ca357c78f2af9ee535d0ed
Roberto Angelo da Silva Ramos <sdinformatia@gmail.com>
2021-01-03 21:39:11 -0300

    criando entidade usuario

---
+++ src/main/java/com/sdinfor/controlefinancas/model/entity/Usuario.java
@@ -0,0 +1,32 @@
+package com.sdinfor.controlefinancas.model.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuario", schema = "financas")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "senha")
+    private String senha;
+}
