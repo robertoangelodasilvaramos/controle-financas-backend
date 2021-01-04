614e279b1c8c9427a1daa9ba140b8b6acc5b5aa2
Roberto Angelo da Silva Ramos <sdinformatia@gmail.com>
2021-01-03 21:48:31 -0300

    criando entidade lancamento

---
+++ src/main/java/com/sdinfor/controlefinancas/model/entity/Lancamento.java
@@ -0,0 +1,38 @@
+package com.sdinfor.controlefinancas.model.entity;
+
+
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "lancamento", schema = "financas")
+public class Lancamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "mes")
+    private Integer mes;
+
+    @Column(name = "ano")
+    private Integer ano;
+
+    @JoinColumn(name = "usuario_id")
+    @ManyToOne
+    private Usuario usuario;
+
+    @Column(name = "valor")
+    private BigDecimal valor;
+
+    @Column(name = "data_cadastro")
+    @Convert(converter = Jsr310JpaConverters.LocalDateConverter.class)
+    private LocalDate dataCadastro;
+}
